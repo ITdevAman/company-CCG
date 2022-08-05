fbbab073ba998f983e0a1a81857f2732972acd58
ITdevAman <itdeveloperaman@gmail.com>
2022-08-05 21:00:52 +0600

    inital

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import {Routes , Route} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Routes, Route, useLocation} from "react-router-dom";
 import HomePage from "./Helpers/HomePage";
 import ServicesHomePage from "./Helpers/ServicesHomePage";
 
 
 const App = () => {
+    const location = useLocation();
+    const scroolTop = () => {
+      window.scrollTo({
+          left : 0,
+          top : 0,
+          behavior: "smooth"
+      })
+    }
+    useEffect(()=>{
+        scroolTop()
+    }, [location.pathname])
+
     return (
         <Routes>
             <Route path={"/"} element={<HomePage/>}/>
